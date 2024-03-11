4ebb06aa1b9a983d30c1b0134dac0b61249c778e
Inesbourguibaweb <inesbourguibaweb@gmail.com>
2024-03-11 19:45:58 +0100

    add preloading to improve lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModel } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppinListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -19,11 +14,8 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModel,
     HttpClientModule,
-    RecipesModule,
-    ShoppinListModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
 
   bootstrap: [AppComponent],
